8883c3c485924ec7252055b39bb02e01334bb97a
dinesh10303 <avuladineshreddy@gmail.com>
2025-11-22 18:04:00 +0530

    updated code

--- terraform/network.tf
+++ terraform/network.tf
@@ -17,7 +17,7 @@ resource "google_compute_subnetwork" "public" {
 resource "google_compute_subnetwork" "private" {
   name          = "demo-private-subnet"
   ip_cidr_range = "10.0.1.0/24"
-  region        = "us-central"
+  region        = "us-east1"
   network       = google_compute_network.vpc.id
   private_ip_google_access = false
 }
