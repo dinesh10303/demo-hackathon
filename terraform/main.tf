1f1e4ab9adc019cecedcb01954744cb68e817a9a
dinesh10303 <avuladineshreddy@gmail.com>
2025-11-22 16:31:02 +0530

    updated code

---
+++ terraform/main.tf
@@ -0,0 +1,3 @@
+provider "google" {
+    project = "glossy-motif-478215-t4"
+}
---
+++ terraform/network.tf
@@ -0,0 +1,23 @@
+# Create VPC
+resource "google_compute_network" "vpc" {
+  name                    = demo-vpc
+  auto_create_subnetworks = false
+}
+
+# Public Subnet
+resource "google_compute_subnetwork" "public" {
+  name          = "demo-public-subnet"
+  ip_cidr_range = 10.0.0.0/24
+  region        = "us-east1"
+  network       = google_compute_network.vpc.id
+  private_ip_google_access = true
+}
+
+# Private Subnet
+resource "google_compute_subnetwork" "private" {
+  name          = "demo-private-subnet"
+  ip_cidr_range = 10.0.1.0/24
+  region        = "us-central"
+  network       = google_compute_network.vpc.id
+  private_ip_google_access = true
+}
